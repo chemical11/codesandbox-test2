4b7675f76be8c4026ae2b553f12c4fefab67bd70
chemical <ma17082@shibaura-it.ac.jp>
2023-01-06 06:12:53 +0000

    commit1

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import "./styles.css";
 
+console.log("aaa");
 document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
+<h1>Hello Vanilla!!!</h1>
 <div>
   We use the same configuration as Parcel to bundle this sandbox, you can find more
   info about Parcel 
